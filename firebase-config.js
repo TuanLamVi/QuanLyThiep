7b6ba7ddf1c391878bd2f2c21b54022850fc354a
THIỆP MỜI ONLINE <quychau9x@gmail.com>
2026-05-05 00:57:57 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@
 // Thay YOUR_* bằng config của bạn từ Firebase Console
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBpoi3xuIIiOQXMohVLOITlf0DX9JKbs-4",
+  authDomain: "grouppublic-583ac.firebaseapp.com",
+  projectId: "grouppublic-583ac",
+  storageBucket: "grouppublic-583ac.firebasestorage.app",
+  messagingSenderId: "70167414260",
+  appId: "1:70167414260:web:8d3111a010959a38a965a1"
 };
 
 // Initialize Firebase
